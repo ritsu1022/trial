8f9b56b8c98db367ce8f122869076aa08f3e1b17
ritsu1022 <128396288+ritsu1022@users.noreply.github.com>
2023-10-17 21:11:58 +0900

    first commit

---
+++ trial.c
@@ -0,0 +1,15 @@
+/*
+ * trial.c
+ *
+ *  Created on: 2023/10/17
+ *      Author: 幸子
+ */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("!!!Hello World!!!");
+	return EXIT_SUCCESS;
+}
